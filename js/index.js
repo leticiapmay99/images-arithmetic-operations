4e71937de0bb9284311f07a36fffc4b97c212da7
Leticia P. May <43357500+leticiapmay99@users.noreply.github.com>
2021-05-22 22:10:13 -0300

    Firs commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let imgElement1 = document.getElementById('imgInput1');
+let inputImgElement1 = document.getElementById('fileInput1');
+
+let imgElement2 = document.getElementById('imgInput2');
+let inputImgElement2 = document.getElementById('fileInput2');
+
+// inputImgElement1.addEventListener('change', (e) => {
+//     imgElement1.src = URL.createObjectURL(e.target.files[0]);
+// }, false);
+
+// inputImgElement2.addEventListener('change', (e) => {
+//     imgElement2.src = URL.createObjectURL(e.target.files[0]);
+// }, false);
+
+
+// imgElement1.onload = function () {
+//     let mat = cv.imread(imgElement1);
+//     cv.imshow('canvasOutput', mat);
+//     mat.delete();
+// };
+
+
+// function onOpenCvReady() {
+//     document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+// }
+
+// https://docs.opencv.org/3.4/dd/d4d/tutorial_js_image_arithmetics.html
