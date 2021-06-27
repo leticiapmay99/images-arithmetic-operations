3d52c8586f81151768cd38f7cf48dbf373984ef9
Leticia P. May <43357500+leticiapmay99@users.noreply.github.com>
2021-06-27 20:52:36 -0300

    funcionando até aqui

--- js/index.js
+++ js/index.js
@@ -1,108 +1,128 @@
 // Input Elements
-const imgInputOne  = document.getElementById('imgInputOne');
+const imgInputOne = document.getElementById('imgInputOne');
 const fileInputOne = document.getElementById('fileInputOne');
 
-const imgInputTwo  = document.getElementById('imgInputTwo');
+const imgInputTwo = document.getElementById('imgInputTwo');
 const fileInputTwo = document.getElementById('fileInputTwo');
 
-const canvasOutput = document.getElementById('canvasOutput');
+// const canvasOutput = document.getElementById('canvasOutput');
+const imageOutput = document.getElementById('imageOutput');
 
-const btnSum            = document.getElementById('btnSum');
-const btnSubtraction    = document.getElementById('btnSubtraction');
+const btnSum = document.getElementById('btnSum');
+const btnSubtraction = document.getElementById('btnSubtraction');
 const btnMultiplication = document.getElementById('btnMultiplication');
-const btnDivision       = document.getElementById('btnDivision');
-const btnAverage        = document.getElementById('btnAverage');
-const btnBlending       = document.getElementById('btnBlending');
-const btnAndPort        = document.getElementById('btnAndPort');
-const btnOrPort         = document.getElementById('btnOrPort');
-const btnXorPort        = document.getElementById('btnXorPort');
-const btnNotPort        = document.getElementById('btnNotPort');
+const btnDivision = document.getElementById('btnDivision');
+const btnAverage = document.getElementById('btnAverage');
+const btnBlending = document.getElementById('btnBlending');
+const btnAndPort = document.getElementById('btnAndPort');
+const btnOrPort = document.getElementById('btnOrPort');
+const btnXorPort = document.getElementById('btnXorPort');
+const btnNotPort = document.getElementById('btnNotPort');
 
-// EventListeners
-fileInputOne.addEventListener('change', (e) => {
-  imgInputOne.src = URL.createObjectURL(e.target.files[0]);
-  enableOperations();
+// image-js
+// async function process() {
+//   let image = await IJS.Image.load(imgInputOne.src);
+//   console.log(IJS.divide())
+
+//   // imageOutput.src = grey.toDataURL();
+// }
+
+async function sumImages() {
+
+    const firstImage = await jimp.read(imgInputOne.src);
+    const secondImage = await jimp.read(imgInputTwo.src);
+
+    const result = firstImage.composite(secondImage, 0, 0, {
+        mode: jimp.BLEND_ADD
+    });
+
+    result.getBase64(jimp.AUTO, function (err, data) {
+        imageOutput.src = data
+    })
+
+}
+
+
+async function subtractionImages() {
+    const firstImage = await jimp.read(imgInputOne.src);
+    const secondImage = await jimp.read(imgInputTwo.src);
+
+    const result = firstImage.composite(secondImage, 0, 0, {
+        mode: jimp.BLEND_DIFFERENCE
+    });
+
+    result.getBase64(jimp.AUTO, function (err, data) {
+        imageOutput.src = data
+    })
+
+}
+
+async function multiplyImages() {
+    const firstImage = await jimp.read(imgInputOne.src);
+    const secondImage = await jimp.read(imgInputTwo.src);
+
+    const result = firstImage.composite(secondImage, 0, 0, {
+        mode: jimp.BLEND_MULTIPLY
+    });
+
+    result.getBase64(jimp.AUTO, function (err, data) {
+        imageOutput.src = data
+    })
+
+}
+
+// // EventListeners
+fileInputOne.addEventListener('change', async (e) => {
+    imgInputOne.src = await URL.createObjectURL(e.target.files[0]);
 }, false);
 
-fileInputTwo.addEventListener('change', (e) => {
-  imgInputTwo.src = URL.createObjectURL(e.target.files[0]);
-  enableOperations();
+fileInputTwo.addEventListener('change', async (e) => {
+    imgInputTwo.src = await URL.createObjectURL(e.target.files[0]);
 }, false);
 
 btnSum.addEventListener('click', () => {
-  sumImages(imgInputOne, imgInputTwo)
+    sumImages()
 });
+
+
+btnMultiplication.addEventListener('click', () => {
+    multiplyImages()
+});
+
 
 btnSubtraction.addEventListener('click', () => {
-  subtractionImages(imgInputOne, imgInputTwo)
+    subtractionImages()
 });
+const appStatus = function () {
+    const status = document.getElementById('status');
+    status.classList.add('ready')
+    status.innerHTML = 'Jimp is ready!';
 
-// Operations
-const sumImages = function(imgOneId, imgTwoId) {
-  if (fileInputOne.value !== '' || fileInputTwo.value !== '') {
-    let src1 = cv.imread(imgOneId);
-    let src2 = cv.imread(imgTwoId);
-    let dst = new cv.Mat();
-    let mask = new cv.Mat();
-    let dtype = -1;
-    cv.add(src1, src2, dst, mask, dtype);
-    cv.imshow(canvasOutput, dst);
-    src1.delete();
-    src2.delete();
-    dst.delete();
-    mask.delete();
-  }
+    this.setTimeout(() => {
+        status.classList.add('hidden')
+    }, 1500);
 }
 
-const subtractionImages = function(imgOneId, imgTwoId) {
-  if (fileInputOne.value !== '' || fileInputTwo.value !== '') {
-    let src1 = cv.imread(imgOneId);
-    let src2 = cv.imread(imgTwoId);
-    let dst = new cv.Mat();
-    let mask = new cv.Mat();
-    let dtype = -1;
-    cv.subtract(src1, src2, dst, mask, dtype);
-    cv.imshow(canvasOutput, dst);
-    src1.delete();
-    src2.delete();
-    dst.delete();
-    mask.delete();
-  }
-}
-
-const enableOperations = function() {
-  if (!fileInputOne.value || !fileInputTwo.value) return;
-
-  const buttons = document.querySelectorAll('.list-block .list-buttons button');
-
-  buttons.forEach(btn => {
-    btn.removeAttribute('disabled')
-  });
-}
-
-const appStatus = function() {
-  const status = document.getElementById('status');
-  status.classList.add('ready')
-  status.innerHTML = 'OpenCV.js is ready!';
-  
-  this.setTimeout(() => {
-    status.classList.add('hidden')
-  }, 1500);
-}
-
-/** 
- * Verifica e dispara eventos se o DOM carregou
- * https://devanilla.com/articles/on-load-vanilla-javascript
- * https://docs.opencv.org/3.4/dd/d4d/tutorial_js_image_arithmetics.html
-**/
 window.addEventListener('load', appStatus);
 
 
-/** 
- * Exemplos encontrados
+/**
+ * Jimp
+ * https://www.npmjs.com/package/jimp
+ * https://www.youtube.com/watch?v=Wk3gkFNC0s8
+ * https://www.youtube.com/watch?v=2H7pRypEeD0
+ * https://imasters.com.br/front-end/processamento-de-imagens-em-javascript
+ * https://www.tabnine.com/code/javascript/functions/jimp/Jimp/write
+ *
+ * Image JS
+ * https://image-js.github.io/image-js/#imagesum
+ * https://www.npmjs.com/package/image-js
+ * https://github.com/image-js/image-js
+ *
+ * OpenCV
  * https://www.ccoderun.ca/programming/doxygen/opencv/tutorial_js_image_arithmetics.html
  * https://docs.opencv.org/3.4/dd/d4d/tutorial_js_image_arithmetics.html
  * - Blending: https://codepen.io/huningxin/pen/GEGEWM
  * - https://github.com/haoking/opencvjs
- * - 
+ * -
 **/
