55a3a2b8bd3f2b68605560464dce1b78206d5a2d
Matheus Fabian <43357502+matheusfelipefabian@users.noreply.github.com>
2021-07-06 21:59:48 -0300

    fix size of load images

--- js/index.js
+++ js/index.js
@@ -14,13 +14,21 @@ fileInputOne.addEventListener('change', handleFiles);
 fileInputTwo.addEventListener('change', handleFiles2);
 
 function handleFiles(e) {
-	console.log(imgInputOne)
+	console.log('imgInputOne', imgInputOne)
+	console.log('e', e.target.files[0])
     let ctx = imgInputOne.getContext('2d');
     var img = new Image;
     img.src = URL.createObjectURL(e.target.files[0]);
     img.onload = function() {
-		ctx.drawImage(img, 20,20);
-		pixelData = ctx.getImageData(0,0, 200, 200)
+		// imgInputOne.width = 50;
+    	// imgInputOne.height = 85;
+		imgInputOne.setAttribute('width', 250);
+		imgInputOne.setAttribute('height', 285);
+		console.log('window.innerWidth', window.innerWidth)
+		console.log('window.innerHeight', window.innerHeight)
+		drawImageProp(ctx, this, 0, 0, imgInputOne.width, imgInputOne.height);
+		// ctx.drawImage(img, 0,0);
+		pixelData = ctx.getImageData(0,0, imgInputOne.width, imgInputOne.height)
 		console.log(pixelData)
     }
 	ctx.clearRect(0, 0, 265, 285);
@@ -32,9 +40,14 @@ function handleFiles2(e) {
     var img = new Image;
     img.src = URL.createObjectURL(e.target.files[0]);
     img.onload = function() {
-		ctx.drawImage(img, 20,20);
-		pixelData2 = ctx.getImageData(0,0, 200, 200)
-		console.log(pixelData2)
+		imgInputTwo.setAttribute('width', 250);
+		imgInputTwo.setAttribute('height', 285);
+		console.log('window.innerWidth', window.innerWidth)
+		console.log('window.innerHeight', window.innerHeight)
+		drawImageProp(ctx, this, 0, 0, imgInputTwo.width, imgInputTwo.height);
+		// ctx.drawImage(img, 20,20);
+		pixelData2 = ctx.getImageData(0,0, imgInputTwo.width, imgInputTwo.height)
+		// console.log(pixelData2)
     }
 	ctx.clearRect(0, 0, 265, 285);
 }
@@ -134,7 +147,53 @@ function binario(){
 // }
 
 
+function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
 
+    if (arguments.length === 2) {
+        x = y = 0;
+        w = ctx.canvas.width;
+        h = ctx.canvas.height;
+    }
+
+    /// default offset is center
+    offsetX = offsetX ? offsetX : 0.5;
+    offsetY = offsetY ? offsetY : 0.5;
+
+    /// keep bounds [0.0, 1.0]
+    if (offsetX < 0) offsetX = 0;
+    if (offsetY < 0) offsetY = 0;
+    if (offsetX > 1) offsetX = 1;
+    if (offsetY > 1) offsetY = 1;
+
+    var iw = img.width,
+        ih = img.height,
+        r = Math.min(w / iw, h / ih),
+        nw = iw * r,   /// new prop. width
+        nh = ih * r,   /// new prop. height
+        cx, cy, cw, ch, ar = 1;
+
+    /// decide which gap to fill    
+    if (nw < w) ar = w / nw;
+    if (nh < h) ar = h / nh;
+    nw *= ar;
+    nh *= ar;
+
+    /// calc source rectangle
+    cw = iw / (nw / w);
+    ch = ih / (nh / h);
+
+    cx = (iw - cw) * offsetX;
+    cy = (ih - ch) * offsetY;
+
+    /// make sure source rectangle is valid
+    if (cx < 0) cx = 0;
+    if (cy < 0) cy = 0;
+    if (cw > iw) cw = iw;
+    if (ch > ih) ch = ih;
+
+    /// fill image in dest. rectangle
+    ctx.drawImage(img, cx, cy, cw, ch,  x, y, w, h);
+}
 
 
 
